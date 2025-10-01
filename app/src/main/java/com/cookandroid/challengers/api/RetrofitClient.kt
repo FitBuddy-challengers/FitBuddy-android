7fb75f752b4332433d294bb529278b7ee609067d
Chea-yunzi <katete051@naver.com>
2025-10-01 13:18:33 +0900

    chore: 초기 업로드(기존 프로젝트 마이그레이션)

--- app/src/main/java/com/cookandroid/challengers/api/RetrofitClient.kt
+++ app/src/main/java/com/cookandroid/challengers/api/RetrofitClient.kt
@@ -14,7 +14,8 @@ import com.google.gson.annotations.SerializedName
 // 서버와 애뮬레이터 연동을 위한 코드 절대 수정 XXX
 object RetrofitClient {
     // private val BASE_URL: String = BuildConfig.BASE_URL // ★ 타입 명시!
-    private const val BASE_URL = "http://10.0.2.2:3000/"
+    private const val BASE_URL = "http://10.0.2.2:3000/"//<- 원래 내 pc
+    //private const val BASE_URL = "http://172.19.27.182:3000/" //내 갤럭시//172.19.27.182
 
     //  먼저 gson 정의Add commentMore actions
     private val gson = GsonBuilder()
